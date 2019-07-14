12958a3d18f7441dfa4254d90737cc7cdcc5b849
Andrei Tosa <deiu.tosa@gmail.com>
2019-07-14 21:13:41 +0300

    Init project

---
+++ src/components/ContactList.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>{{title}}</h1>
+  </section>
+</template>
+<script>
+export default {
+  name: "ContactList",
+  data() {
+    return {
+      title: "Contact List"
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <ContactList />
+  </div>
+</template>
+
+<script>
+import ContactList from "../components/ContactList";
+
+export default {
+  name: 'home',
+  components: {
+    ContactList
+  }
+}
+</script>
